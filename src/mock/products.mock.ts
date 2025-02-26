4cfb1071c6d48e1ac989155a59606f3ae940d3ed
ruansilva123 <ruancplay59@gmail.com>
2025-02-26 16:54:58 -0300

    :sparkles: feat: init home page - add images for footer, hero and header; - create mock base; - create variables for colors of site.

---
+++ src/mock/products.mock.ts
@@ -0,0 +1,14 @@
+export const MOCK_PRODUCTS = [
+    {
+        id: 1,
+        name: "Camisa Space",
+        price: 69.99,
+        sizes: ["p", "m", "g"],
+        description: "Camisa branca e azul, com tema espacial.",
+        images: [
+            "imagem 1",
+            "imagem 2",
+            "imagem 3"
+        ]
+    },
+]
