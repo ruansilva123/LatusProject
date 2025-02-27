a6acf43feae62a40e293340ed3f04c2aadd26733
ruansilva123 <ruancplay59@gmail.com>
2025-02-27 16:55:34 -0300

    :wastebasket: chore: corrected responvive div cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RedirectButtonComponent } from '../../components/redirect-button/redirect-button.component';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { MOCK_PRODUCTS } from '../../../mock/products.mock';
 
 @Component({
   selector: 'app-home',
@@ -12,5 +13,5 @@ import { ProductCardComponent } from '../../components/product-card/product-card
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  products = MOCK_PRODUCTS;
 }
--- src/mock/products.mock.ts
+++ src/mock/products.mock.ts
@@ -3,13 +3,55 @@ export const MOCK_PRODUCTS = [
         id: 1,
         name: "Camisa Space",
         price: 69.99,
-        sizes: ["p", "m", "g"],
+        sizes: ["P", "M", "G"],
         category: "JDM",
         description: "Camisa branca e azul, com tema espacial.",
         images: [
-            "imagem 1",
-            "imagem 2",
-            "imagem 3"
+            "product-images/camisa-space.png"
+        ]
+    },
+    {
+        id: 2,
+        name: "Camisa Space 2",
+        price: 69.99,
+        sizes: ["P", "M", "G"],
+        category: "JDM",
+        description: "Camisa branca e azul, com tema espacial.",
+        images: [
+            "product-images/camisa-space.png"
+        ]
+    },
+    {
+        id: 3,
+        name: "Camisa Space 3",
+        price: 69.99,
+        sizes: ["P", "M", "G"],
+        category: "JDM",
+        description: "Camisa branca e azul, com tema espacial.",
+        images: [
+            "product-images/camisa-space.png"
+        ]
+    },
+    {
+        id: 4,
+        name: "Camisa Space 4",
+        price: 69.99,
+        sizes: ["P", "M", "G", "GG"],
+        category: "JDM",
+        description: "Camisa branca e azul, com tema espacial.",
+        images: [
+            "product-images/camisa-space.png"
+        ]
+    },
+    {
+        id: 5,
+        name: "Camisa Space 5",
+        price: 69.99,
+        sizes: ["P", "M", "G"],
+        category: "JDM",
+        description: "Camisa branca e azul, com tema espacial.",
+        images: [
+            "product-images/camisa-space.png"
         ]
     },
 ]
